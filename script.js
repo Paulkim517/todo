60c7517118950b2298c5cdffb1d0e9dbfefecf80
Paul Kim <paulkim517@gmail.com>
2015-07-01 19:34:01 -0700

    refactored the JS page to be OOP.

--- script.js
+++ script.js
@@ -2,12 +2,41 @@ $(function() {
 
   // `toDos` array is our model (holds our data)
   // contains test (or "seed") data
-  var toDos = [
-  {task: "laundry", description: "clean clothes"},
-  {task: "grocery shopping", description: "buy food"},
-  {task: "nap time", description: "remember to sleep!"}
-  ];
-  console.log(toDos);
+  var whatever = [{taske: "laundry", description: "clean clothes"},
+  {taske: "grocery shopping", description: "buy food"},
+  {taske: "nap time", description: "remember to sleep!"}];
+  
+
+  var toDoers = function(taske, description){
+
+    this.taske = taske;
+    this.description = description;
+
+
+  }
+  this.all = []
+
+  toDoers.prototype.save = function(){
+
+    this.all.push(this)
+
+  }
+
+  toDoers.prototype.render = function (){
+
+    console.log(this.taske)
+
+    // append our seed todo to the page
+    var $todo = $(todoTemplate(this));
+    console.log(this.taske)
+    $todo.attr(this.taske , this.description);
+    console.log ("hello world")
+    $toDoList.append($todo);
+
+  }
+
+  
+  //console.log(toDos);
 
    // form to create new todo
    var $newToDo = $('#new-todo');
@@ -15,13 +44,13 @@ $(function() {
   // element to hold our list of todos
   var $toDoList = $('#todo-list');
 
-// underscore template
-var todoTemplate = _.template($('#todo-template').html());
-console.log(todoTemplate)
+   // underscore template
+    var todoTemplate = _.template($('#todo-template').html());
+    console.log(todoTemplate)
 
 //seed data append
-_.each(toDos, function (toDos, index) {
-	var $todo = $(todoTemplate(toDos));
+_.each(whatever, function (toDoers, index) {
+	var $todo = $(todoTemplate(toDoers));
 	//$todo.attr('data-index', index);
 	$toDoList.append($todo);
 });
@@ -34,19 +63,22 @@ _.each(toDos, function (toDos, index) {
     // create new todo object from form data
     var toDoName = $('#todo-name').val();
     var toDoDesc = $('#todo-desc').val();
-    var toDoData = {task: toDoName, description: toDoDesc};
+    var toDoData = new toDoers(toDoName,toDoDesc);
 
     // store our new todo
-    toDos.push(toDoData);
-    var index = toDos.indexOf(toDoData);
-    console.log(toDos)
+    // toDoers.push(toDoData);
+    // toDoData.save();
+    // var index = whatever.indexOf(toDoData);
+    // console.log(toDoers)
 
     // append our new todo to the page
-   var $todo = $(todoTemplate(toDoData));
-	$todo.attr('data-index', index);
-	 console.log ($todo)
-	$toDoList.append($todo);
-   
+    // var $todo = $(todoTemplate(toDoData));
+    // $todo.attr('data-index', index);
+    // console.log ($todo)
+    // $toDoList.append($todo);
+
+    toDoData.render();
+
     // reset the form
     $newToDo[0].reset();
     $('#todo-name').focus();
